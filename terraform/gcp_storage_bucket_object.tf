f54726f90ab34354f3ed49601dfb20ac9a375ef2
DarkLast98 <danielalbertoquinterocastro@gmail.com>
2021-09-11 17:09:55 -0500

    bucket name configuration

--- terraform/gcp_storage_bucket_object.tf
+++ terraform/gcp_storage_bucket_object.tf
@@ -5,6 +5,6 @@
  */
 resource "google_storage_bucket_object" "archive" {
   name   = "meli-functions.zip${data.archive_file.source.output_md5}"
-  bucket = google_storage_bucket.bucket.name
+  bucket = google_storage_bucket.functions.name
   source = data.archive_file.source.output_path
 }
