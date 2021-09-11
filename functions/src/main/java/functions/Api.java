6a17e654ea71539768c0d0289205f4a6a3290bdd
DarkLast98 <danielalbertoquinterocastro@gmail.com>
2021-09-11 18:26:21 -0500

    add .idea ignore

--- functions/src/main/java/functions/Api.java
+++ functions/src/main/java/functions/Api.java
@@ -6,23 +6,12 @@ import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-// public class Api implements HttpFunction {
-//   // Simple function to return "Hello World"
-//   @Override
-//   public void service(HttpRequest request, HttpResponse response)
-//       throws IOException {
-//     BufferedWriter writer = response.getWriter();
-//     writer.write("Hello World!");
-//   }
-  @SpringBootApplication
-  public class Api {
-    
-    public static void main(String[] args) {
-      SpringApplication.run(CloudFunctionMain.class, args);
-    }
-  
-    @Bean
-    public Function<String, String> uppercase() {
-      return value -> value.toUpperCase();
-    }
+ public class Api implements HttpFunction {
+   // Simple function to return "Hello World"
+   @Override
+   public void service(HttpRequest request, HttpResponse response)
+       throws IOException {
+     BufferedWriter writer = response.getWriter();
+     writer.write("Hello World!");
+   }
 }
