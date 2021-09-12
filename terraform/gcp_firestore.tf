8f4c6529dfb7b140bd13c658507f45614f1d92b3
DarkLast98 <danielalbertoquinterocastro@gmail.com>
2021-09-11 19:10:21 -0500

    firestore configuration

--- terraform/gcp_firestore.tf
+++ terraform/gcp_firestore.tf
@@ -24,6 +24,6 @@ resource "google_app_engine_application" "firestore" {
   database_type = "CLOUD_FIRESTORE"
 
   depends_on = [
-     google_project_service.firestore
+     google_project_service.firestore_services
   ]
 }
