9b6a3a7de3c43f173dc3b8014a6c045ec24e2cf8
DarkLast98 <danielalbertoquinterocastro@gmail.com>
2021-09-12 00:12:43 -0500

    update package

--- functions/src/main/java/com/meli/functions/Api.java
+++ functions/src/main/java/com/meli/functions/Api.java
@@ -3,7 +3,7 @@ package com.meli.functions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.meli.dnaanalizer")
+@SpringBootApplication(scanBasePackages = "com.meli.functions")
 public class Api {
 
 	public static void main(String[] args) {
