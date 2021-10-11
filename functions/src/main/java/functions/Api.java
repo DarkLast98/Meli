ce2175158bcb2d7d87143514125b6de05a6591bb
DarkLast98 <danielalbertoquinterocastro@gmail.com>
2021-10-11 16:36:06 -0500

    configurations

--- functions/src/main/java/functions/Api.java
+++ functions/src/main/java/functions/Api.java
@@ -1,17 +1,11 @@
 package functions;
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Api implements HttpFunction {
-	// Simple function to return "Hello World"
-	@Override
-	public void service(HttpRequest request, HttpResponse response)
-		throws IOException {
-	  BufferedWriter writer = response.getWriter();
-	  writer.write("Hello World!");
-	}
-  }
+@SpringBootApplication
+public class Api{
+	public static void main(String[] args) {
+		SpringApplication.run(Api.class, args);
+	  }
+}
